4df04d4943bdce2f1b53cbb83cf0086dcda01b0e
axan.iqbal <axan.iqbal1234@gmail.com>
2020-09-02 18:39:02 +0500

    Revert "Revert "Revert "Revert "First Commit""""
    This reverts commit c20714efe0e4254c75d9126c6ea30e7c9354ea42.

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:meal/widgets/meal_item.dart';
+
+import '../models/meal.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('you have no favorites yet - start adding some!'),
+      );
+    }
+    return ListView.builder(
+      itemBuilder: (ctx, index) {
+        return MealItem(
+          title: favoriteMeals[index].title,
+          imageUrl: favoriteMeals[index].imageUrl,
+          duration: favoriteMeals[index].duration,
+          complexity: favoriteMeals[index].complexity,
+          affordability: favoriteMeals[index].affordability,
+          id: favoriteMeals[index].id,
+        );
+      },
+      itemCount: favoriteMeals.length,
+    );
+  }
+}
